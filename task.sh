486e607ad4f6866bb191ec585ed3ef8ac2ad7d25
Yogesh <52989607+yogesh2k21@users.noreply.github.com>
2022-05-21 16:14:56 +0530

    task.sh is added.
    This `task.sh` file is used for only `UBUNTU`.
    By only executing this file in Linux we can run this project directly on the host machine.

---
+++ task.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo snap install docker
+
+git clone https://github.com/yogesh2k21/Weather-App
+
+cd /home/ubuntu/Weather-App
+
+#display current directory path
+echo $(pwd)
+
+#docker-compose.yml run command
+sudo docker-compose -f docker-compose.yml up -d
